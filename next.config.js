998936a319030ffeb219c0c106c1ffe964df28cd
liseezn <mail@liseezn.top>
2026-04-12 19:26:28 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 默认配置即可，无需额外内容
+    // 启用 Turbopack (Next.js 16 默认开启，此处显式声明)
+    turbopack: {},
+    // 如果你想继续使用 Webpack，可以删除 turbopack 配置
 };
 
 module.exports = nextConfig;
